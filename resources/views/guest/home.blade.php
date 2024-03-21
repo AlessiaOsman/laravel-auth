647696083f03092e0c329775a19c3866327be1f9
AlessiaOsman <alessia.osman@gmail.com>
2024-03-21 08:21:25 +0100

    added slug

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -19,17 +19,6 @@
     <div class="card-body">
       <h5 class="card-title mb-4"><a href="{{ $project->url }}">{{ $project->url }}</a></h5>
       <p class="card-text mb-4">{{ $project->content }}</p>
-      <div class="d-flex justify-content-between">
-      <a href="{{ route('adminprojects.index', $project) }}" class="btn btn-primary"><i
-        class="fa-solid fa-arrow-left me-2"></i>Torna indietro</a>
-        
-        <form action="{{route('adminprojects.destroy', $project)}}" method="POST" class="delete-form">
-            @csrf
-            @method('DELETE')
-            <button  type="submit" class="btn btn-danger "><i class="fa-solid fa-trash-can me-2"></i>Elimina</button>
-        </form>
-
-    </div>  
     </div>
   </div>
   @empty
